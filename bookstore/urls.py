a13fdb68ad004473a4418531cac94259267e40bf
kottu-mouli <moulisaiprasad31@gmail.com>
2026-05-18 10:04:11 +0530

    Add Render deployment config

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,11 +3,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
+from books.views import BookViewSet
+from communities.views import CommunityViewSet, EventViewSet
+
+router = DefaultRouter()
+router.register('books', BookViewSet)
+router.register('communities', CommunityViewSet)
+router.register('events', EventViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('books.urls')),
-    path('api/', include('communities.urls')),
+    path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
